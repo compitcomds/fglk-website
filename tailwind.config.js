e946be382fe885c43f32cf5dadcf3a55df1c605b
Dpsharma@1223 <manoj.sharma@matacube.com>
2024-03-29 13:02:15 +0530

    hygkulllllllllllllll

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    extend: {
+      // colors: {
+      //   primary:"#fffbf7",
+      //   secondaryp:"#fed1fa",
+      //   secondaryo:"#fb6242",
+      //   secondaryg: "#ceec87",
+      //   button: "#e87b38",
+      //   buttonhover: "#f8c23a",
+      // },
+      colors: {
+        primary:"#0b0b2b",
+        secondaryp:"#fed1fa",
+        secondaryo:"#fb6242",
+        secondaryg: "#ceec87",
+        button: "#e87b38",
+        buttonhover: "#f8c23a",
+        textcol : "#ffffff"
+      },
+      fontFamily: {
+        
+        webFont: ['Raleway', 'sans-serif'],
 
+      },
+    },
+    plugins: [],
+  },
+};
