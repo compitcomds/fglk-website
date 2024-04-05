3342f98f15fa1c5f82c3d293cea4b33d98133801
compitcomds <compitcomds@gmail.com>
2024-04-05 21:56:13 +0530

    db

--- fglk/static/js/app.js
+++ fglk/static/js/app.js
@@ -140,7 +140,7 @@ window.onload = function () {
         spaceBetween: 30,
         loop: true,
         autoplay: {
-            delay: 300000,
+            delay: 3000,
             disableOnInteraction: false, // Change to false to keep autoplay running even when user interacts with swiper
         },
         createElements: false,
