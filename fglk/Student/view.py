17e329ee68c517fa05bc56ddbcb008315ecd04af
compitcomds <compitcomds@gmail.com>
2024-04-25 16:12:26 +0530

    hjkl

---
+++ fglk/Student/view.py
@@ -0,0 +1,16 @@
+from flask import Blueprint,render_template,redirect,url_for,request,flash
+from flask_login import logout_user, login_required, current_user
+from fglk.database import db 
+from bson.objectid import ObjectId
+
+
+Student = Blueprint('Student',__name__,template_folder='templates/Student',static_folder='static/Student')
+
+# @Student.before_request
+# @login_required
+# def checker():
+#       cookie_token = request.cookies.get('token')
+#       if current_user and current_user.token != cookie_token:
+#                 logout_user()
+#                 flash('You have been logged out due to multiple logins.', 'error')
+#                 return redirect(url_for('Auth.login'))
